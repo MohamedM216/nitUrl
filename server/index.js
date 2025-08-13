78dbcac44621b52c41497fedfff315e610f1c103
Mohammad El-Shennawy <mohamedwork216@gmail.com>
2025-08-13 09:14:47 +0300

    initialize express server

---
+++ server/index.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const { process } = require('process');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(compression());
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use((req, res, next) => {
+  if (req.header('x-forwarded-proto') !== 'https' && process.env.NODE_ENV === 'production') {
+    res.redirect(`https://${req.header('host')}${req.url}`);
+  } else {
+    next();
+  }
+});
+
+app.get('/health', (req, res) => {
+  res.json({ 
+    status: 'success',
+    message: 'Hello from the backend!',
+  });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running in port: ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
